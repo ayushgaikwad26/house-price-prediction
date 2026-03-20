6572f4016c7ed0a484ac45e450a5b49e7fd939b1
Ayush gaikwad <129099554+ayushgaikwad26@users.noreply.github.com>
2026-03-20 17:00:59 +0530

    Update input labels for clarity in app.py

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 import streamlit as st
 
-st.title("🏠 House Price Prediction")
+st.title("House Price Prediction")
 
-sqft = st.number_input("Enter Square Feet")
-bath = st.number_input("Enter Number of Bathrooms")
-bhk = st.number_input("Enter BHK")
+sqft = st.number_input("Square Feet")
+bath = st.number_input("Bathrooms")
+bhk = st.number_input("BHK")
 
 if st.button("Predict"):
     price = sqft * 0.01 + bath * 10 + bhk * 5
